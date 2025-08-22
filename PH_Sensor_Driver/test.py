414cda456894ed096f71b2b88c45c3fd0642854b
crelle490 <christian.clt@gmail.com>
2025-08-22 15:25:04 +0200

    Integration of pH sensor

---
+++ PH_Sensor_Driver/test.py
@@ -0,0 +1,11 @@
+from PH_sensor import SerialConfig, PHAnalyzer
+
+cfg = SerialConfig(port="/dev/ttyUSB0", baudrate=9600, parity="N", stopbits=1, timeout=1.0)
+sensor = PHAnalyzer(cfg, device_id=1)  # unit = “网络节点” in the instrument menu
+
+sensor.open()
+try:
+    data = sensor.read_all()
+    print(data)
+finally:
+    sensor.close()
